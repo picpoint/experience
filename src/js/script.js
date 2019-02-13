4182db2c4ed011ddd5297ea739e7d1dd7995fd8f
picpoint <picpoint@yandex.ru>
2019-02-13 16:51:13 +0300

    work to move chat

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,24 @@
 //создаем переменную и туда записываем наш элемент в документе
 var head = document.querySelector('.chatline__head');
+var chatline = document.querySelector('.chatline');
 
 //добавляем событие mousedown на данном элементе
 // и передаём туда ф-ию определения координат на документе
 head.addEventListener('mousedown', function (e) {
+	chatline.style.position = 'absolute';
 	getCoordinatsXY(e);
+	document.body.appendChild(head);
 });
 
+head.addEventListener('mousemove', function (e) {
+	getCoordinatsXY(e);
+	chatline.clientX = e.pageX;
+	chatline.clientY = e.pageY;
+});
+
+head.addEventListener('mouseup', function () {
+
+});
 
 //функция определения координат курсора документа
 function getCoordinatsXY (e) {
@@ -14,6 +26,4 @@ function getCoordinatsXY (e) {
 	this.pageY = e.pageY;
 	console.log(this.pageX + ' ' + this.pageY);
 }
-
-
 
