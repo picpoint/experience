4ed49fc13666296254d22202398d2e71d80efb9f
rmtar <picpoint@yandex.ru>
2019-02-15 21:34:12 +0300

    addremoveEventListener

--- src/js/script.js
+++ src/js/script.js
@@ -2,40 +2,30 @@
 var head = document.querySelector('.chatline__head');
 var chatline = document.querySelector('.chatline');
 
-
+// функция получения координат положения
 function getCoordinatsXY (e) {
 	chatline.style.left = e.pageX - chatline.offsetWidth / 2 + 'px';
 	chatline.style.top = e.pageY - chatline.offsetHeight / 15 + 'px';
 }
 
 
+// событие возникающее при нажатии на хедере формы
 head.addEventListener('mousedown', function (e) {
-	head.style.cursor = 'grabbing';
-	chatline.style.position = 'absolute';
-	getCoordinatsXY(e);
-	document.body.appendChild(chatline);
+	head.style.cursor = 'grabbing';   // при нажатии, меняет форму курсора
+	chatline.style.position = 'absolute'; // меняет позиционирование всей формы
+	getCoordinatsXY(e); // при срабатывании нажатия ф-ия получает координаты формы
+	document.body.appendChild(chatline); // форма добавляется в боди
 
 	head.addEventListener('mousemove', function (e) {
-		getCoordinatsXY(e);
-		e.stopPropagation();
+		getCoordinatsXY(e); // ф-ия mousemove запукается при работе ф-ии mousedown
+												// и getCoordinatsXY(e) получает координаты положения
 	});
 
 });
 
-
-head.addEventListener('mouseup', function (e) {
-	chatline.style.position = 'fixed';
-	head.style.cursor = 'grab';
-
-	head.addEventListener('mousemove', function (e) {
-		chatline.style.left = e.pageX - chatline.offsetWidth / 2 + 'px';
-		chatline.style.top = null;
-		e.stopPropagation();
-	});
+// ф-ия срабатывает при отпускании клавиши
+head.addEventListener('mouseup', function () {
+	document.removeEventListener('mousemove', getCoordinatsXY(e)); // по твоему примеру добавил
+																																// ф-ию удаления обработчика
 });
 
-/*
-chatline.ondragstart = function () {
-	return false;
-};
-*/
