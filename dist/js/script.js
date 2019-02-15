89fc5a8179b16247de8c35f63f9a4aeaefea3ac7
rmtar <picpoint@yandex.ru>
2019-02-16 01:19:20 +0300

    add function moving form finallgit status!

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,31 +1,18 @@
-
 var head = document.querySelector('.chatline__head');
 var chatline = document.querySelector('.chatline');
 
-// функция получения координат положения
-function getCoordinatsXY (e) {
+var getXY = function getCoordinatsXY (e) {
 	chatline.style.left = e.pageX - chatline.offsetWidth / 2 + 'px';
 	chatline.style.top = e.pageY - chatline.offsetHeight / 15 + 'px';
+	console.log('MD');
 }
 
-
-// событие возникающее при нажатии на хедере формы
-head.addEventListener('mousedown', function (e) {
-	head.style.cursor = 'grabbing';   // при нажатии, меняет форму курсора
-	chatline.style.position = 'absolute'; // меняет позиционирование всей формы
-	getCoordinatsXY(e); // при срабатывании нажатия ф-ия получает координаты формы
-	document.body.appendChild(chatline); // форма добавляется в боди
-
-	head.addEventListener('mousemove', function (e) {
-		getCoordinatsXY(e); // ф-ия mousemove запукается при работе ф-ии mousedown
-												// и getCoordinatsXY(e) получает координаты положения
-	});
-
+head.addEventListener('mousedown', function () {
+	head.style.cursor = 'grabbing';
+	document.addEventListener('mousemove', getXY);
 });
 
-// ф-ия срабатывает при отпускании клавиши
 head.addEventListener('mouseup', function () {
-	document.removeEventListener('mousemove', getCoordinatsXY(e)); // по твоему примеру добавил
-																																// ф-ию удаления обработчика
+	head.style.cursor = 'grab';
+	document.removeEventListener('mousemove', getXY);
 });
-
