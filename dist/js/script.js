56074bc0f85d7baa747b76e44be3f90d08ff76ac
rmtar <picpoint@yandex.ru>
2019-02-18 23:19:27 +0300

    work with shiftXY head

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,24 +6,32 @@ var getXY = function getCoordinatsXY (e) {
 	chatline.style.top = e.pageY - chatline.offsetHeight / 15 + 'px';
 }
 
+//head.addEventListener('mousedown', shiftXY);
 head.addEventListener('mousedown', function (e) {
 	head.style.cursor = 'grabbing';
-	var shiftX = e.pageX - getCoordsHeader(head).left;
-	var shiftY = e.pageY - getCoordsHeader(head).top;
 	document.addEventListener('mousemove', getXY);
-	
 });
+
 
 head.addEventListener('mouseup', function () {
 	head.style.cursor = 'grab';
 	document.removeEventListener('mousemove', getXY);
 });
 
-//ф-ия получения координат хедера
+//ф-ия получения размера элемента и его позиции относительно окна
 function getCoordsHeader (elem) {
 	var boxHeader = elem.getBoundingClientRect();
 	return {
 		top: boxHeader.top + pageYOffset,
 		left: boxHeader.left + pageXOffset
 	}
+}
+
+// ф-ия получения координать клика на элементе
+function shiftXY (e) {
+	var shiftX = e.pageX - getCoordsHeader(head).left;
+	var shiftY = e.pageY - getCoordsHeader(head).top;
+	console.log('X = ' + shiftX);
+	console.log('Y = ' + shiftY);
+	console.log('-----------------');
 }
