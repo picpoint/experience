bf3d5c5796df2509b180363848bae0866d5b849a
rmtar <picpoint@yandex.ru>
2019-02-19 22:22:37 +0300

    work to shiftXY position

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,22 +1,11 @@
+
 var head = document.querySelector('.chatline__head');
 var chatline = document.querySelector('.chatline');
 
 var getXY = function getCoordinatsXY (e) {
-	chatline.style.left = e.pageX - chatline.offsetWidth / 2 + 'px';
-	chatline.style.top = e.pageY - chatline.offsetHeight / 15 + 'px';
+	chatline.style.left = e.pageX - shiftXY(e).shiftX + 'px';
+	chatline.style.top = e.pageY - shiftXY(e).shiftY + 'px';
 }
-
-//head.addEventListener('mousedown', shiftXY);
-head.addEventListener('mousedown', function (e) {
-	head.style.cursor = 'grabbing';
-	document.addEventListener('mousemove', getXY);
-});
-
-
-head.addEventListener('mouseup', function () {
-	head.style.cursor = 'grab';
-	document.removeEventListener('mousemove', getXY);
-});
 
 //ф-ия получения размера элемента и его позиции относительно окна
 function getCoordsHeader (elem) {
@@ -29,9 +18,21 @@ function getCoordsHeader (elem) {
 
 // ф-ия получения координать клика на элементе
 function shiftXY (e) {
-	var shiftX = e.pageX - getCoordsHeader(head).left;
-	var shiftY = e.pageY - getCoordsHeader(head).top;
-	console.log('X = ' + shiftX);
-	console.log('Y = ' + shiftY);
-	console.log('-----------------');
+	var obj = {
+		shiftX: e.pageX - getCoordsHeader(head).left,
+		shiftY: e.pageY - getCoordsHeader(head).top
+	};
+	return obj;
 }
+
+
+head.addEventListener('mousedown', function (e) {
+	head.style.cursor = 'grabbing';
+	document.addEventListener('mousemove', getXY);
+});
+
+
+head.addEventListener('mouseup', function () {
+	head.style.cursor = 'grab';
+	document.removeEventListener('mousemove', getXY);
+});
