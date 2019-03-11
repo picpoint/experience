91c8534a106d11eccd88a9960c5ab37357043e6d
rmtar <picpoint@yandex.ru>
2019-03-11 21:15:56 +0300

    err to close/open if forms moving to page

--- dist/js/script.js
+++ dist/js/script.js
@@ -79,17 +79,14 @@ function openCloseChatline (e) {
 	console.log(e.currentTarget.classList);
 	console.log('------------------------');
 
-	if ( (e.target.tagName === 'SPAN') && (e.target.parentNode.classList === 'chatline__close') || (e.currentTarget.classList.value === 'chatline__close')) {
-        chatline.style.border = '1px solid red';
-        chatline.style.bottom = '-500px';
-        chatline.style.right = '5px';
-        chatline.style.top = '';
-        chatline.style.left = '';
-	} else if (e.currentTarget.classList.value === 'chatline__close rotate') {
-        chatline.style.border = '1px solid green';
-        chatline.style.bottom = '0';
-    }
-
+	if ((e.target.tagName === 'SPAN') && (e.target.parentNode.classList === 'chatline__close') || (e.currentTarget.classList.value === 'chatline__close')) {
+		chatline.style.border = '1px solid red';
+		chatline.style.top = '';
+		chatline.style.left = '';
+	} else if (e.currentTarget.classList.value === 'chatline__close rotate' ) {
+		chatline.style.border = '1px solid green';
+		chatline.style.bottom = '0';
+	}
 }
 
 
